e4ebf696216818b65ccfab2c3d78d0a1237464c6
Ali Hamza <ali.hamza@example.com>
2026-03-23 12:34:54 +0500

    initialized task1 file

---
+++ task1_system_monitor.sh
@@ -0,0 +1,14 @@
+# File used to store all system administration logs
+LOG_FILE="system_monitor_log.txt"
+
+# Directory where archived log files will be stored
+ARCHIVE_DIR="ArchiveLogs"
+
+# Critical PIDs that should not be killed
+# PID 1 is init/systemd, $$ is the current shell script process
+CRITICAL_PIDS="1 $$"
+
+# Function to write actions into the log file with a timestamp
+log_action() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOG_FILE"
+}
